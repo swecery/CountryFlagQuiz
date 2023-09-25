ab27f121a5414834baae85015caeac1705fd7577
ceren <cereyild@icloud.com>
2023-09-25 19:26:13 +0200

    finish

--- js/index.js
+++ js/index.js
@@ -3,6 +3,58 @@ console.log('ceren');
 function saveNicknameAndRedirect() {
     var nicknameInput = document.getElementById("nickname");
     var nickname = nicknameInput.value;
+
+    nickname = nickname.trim();
+
+    nickname = nickname.replace(/ö/g, 'o');
+    nickname = nickname.replace(/ä/g, 'a');
+    nickname = nickname.replace(/å/g, 'a');
+
+    
+
+    var punctuationRegex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    if (punctuationRegex.test(nickname)) {
+        var warningBox = document.querySelector(".box-warning p");
+        warningBox.textContent = "Punctuation marks cannot be used.";
+        var boxWarning = document.querySelector(".box-warning");
+        boxWarning.style.display = "block";
+
+        setTimeout(function () {
+            boxWarning.style.display = "none";
+        }, 5000);
+
+        return;
+    }
+    if (nickname.length < 3) {
+        var warningBox = document.querySelector(".box-warning p");
+        warningBox.textContent = "You must use at least 3 letters.";
+        var boxWarning = document.querySelector(".box-warning");
+        boxWarning.style.display = "block";
+
+        setTimeout(function () {
+            boxWarning.style.display = "none";
+        }, 5000);
+
+        return;
+    }
+
+
+
+
+
+
+
+    if (!nickname) {
+        var warningBox = document.querySelector(".box-warning p");
+        warningBox.textContent = "Please enter your nickname.";
+        var boxWarning = document.querySelector(".box-warning");
+        boxWarning.style.display = "block";
+        setTimeout(function () {
+            boxWarning.style.display = "none";
+        }, 5000);
+
+        return;
+    }
     var url = "quiz.html?nickname=" + encodeURIComponent(nickname);
     window.location.href = url;
 }
@@ -14,4 +66,16 @@ document.addEventListener("DOMContentLoaded", function () {
         var nicknameDisplay = document.getElementById("nickname-display");
         nicknameDisplay.textContent = "Start Quiz " + nickname + "!";
     }
+    function displayQuizInfo() {
+        var quizInfo = document.getElementById("quiz-info");
+        quizInfo.style.display = "block";
+    }
+
 });
+;
+
+
+function hideQuizInfo() {
+    var quizInfo = document.querySelector('.quiz-info');
+    quizInfo.style.display = 'none';
+}
--- js/quiz.js
+++ js/quiz.js
@@ -1,8 +1,8 @@
 
 $(document).ready(function () {
     var currentQuestion = 0;
-    var score = 0; 
-    var countdown = 15; 
+    var score = 0;
+    var countdown = 15;
     var countdownInterval;
     function startCountdown() {
         countdown = 15;
@@ -11,32 +11,39 @@ $(document).ready(function () {
             $("#countdown").text(countdown);
 
             if (countdown === 0) {
-                clearInterval(countdownInterval); 
-                checkAnswer(-1); 
+                clearInterval(countdownInterval);
+                checkAnswer(-1);
             }
-        }, 1000); 
+        }, 1000);
     }
     function displayQuestion() {
-        var current = questions[currentQuestion];
-        $("#question").text(current.question);
-        $("#flag-image").attr("src", current.image);
-        $("#countdown").text(countdown);
-        var answersList = $("#answers").empty();
+        if (currentQuestion >= 0 && currentQuestion < questions.length) {
+            var current = questions[currentQuestion];
+            $("#question").text(current.question);
+            $("#flag-image").attr("src", current.image);
+            $("#countdown").text(countdown);
+            var answersList = $("#answers").empty();
 
-        current.answers.forEach(function (answer, index) {
-            var listItem = $("<li></li>");
-            var answerButton = $("<button class='answer'></button>").text(answer);
-            answerButton.on("click", function () {
-                checkAnswer(index);
+            current.answers.forEach(function (answer, index) {
+                var listItem = $("<li></li>");
+                var answerButton = $("<button class='answer'></button>").text(answer);
+                answerButton.on("click", function () {
+                    checkAnswer(index);
+                });
+
+                listItem.append(answerButton);
+                answersList.append(listItem);
             });
 
-            listItem.append(answerButton);
-            answersList.append(listItem);
-        });
-        startCountdown(); 
+            startCountdown();
+        } else {
+            console.log("Quiz finish.");
+        }
     }
+
+
     function checkAnswer(userAnswerIndex) {
-        clearInterval(countdownInterval); 
+        clearInterval(countdownInterval);
         if (userAnswerIndex === questions[currentQuestion].correctAnswer) {
             score++;
         }
@@ -44,11 +51,37 @@ $(document).ready(function () {
         if (currentQuestion < questions.length) {
             displayQuestion();
         } else {
-            $("#question").text("Quiz Finished! Your Score: " + score);
+            $("#question").text("Quiz Finished! Result: " + score);
             $("#answers").empty();
-            $("#flag-image").attr("src", "");
-            $("#time-remaining").hide(); 
+            $("#flag-image").hide();
+            $("#time-remaining").hide();
+            $(".go-leader").html('<button class="button" id="results-button">See Results</button>');
+            $("#results-button").click(function () {
+                goToResults();
+            });
+            const nickname = window.location.href.split("?")[1].split("=")[1];
+            let data = JSON.parse(localStorage.getItem("userData")) || [];
+            if (!data) {
+                data = [];
+            }
+            data.push({ nickname, score });
+            localStorage.setItem("userData", JSON.stringify(data));
+            console.log(data);
         }
+    }
+    function goToResults() {
+        $(".question-card").css("display", "none");
+        $(".results").css("display", "block");
+        let userData = JSON.parse(localStorage.getItem("userData")) || [];
+        userData.sort(function (a, b) {
+            return b.score - a.score;
+        });
+        var resultHTML = '';
+        userData.forEach(function (user, index) {
+            resultHTML += '<li>' + user.nickname + ' - ' + user.score + ' points</li>';
+        });
+        $(".result-list").html(resultHTML);
+
     }
 
     displayQuestion();
